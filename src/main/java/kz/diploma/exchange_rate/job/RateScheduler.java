13d6b8feb563efe5a16b5f787587f823f882ca6a
sNurseit <nurseit.taimas@gmail.com>
2024-08-16 02:32:47 +0500

    fix scheduler timing, add need annotations

--- src/main/java/kz/diploma/exchange_rate/job/RateScheduler.java
+++ src/main/java/kz/diploma/exchange_rate/job/RateScheduler.java
@@ -4,16 +4,18 @@ package kz.diploma.exchange_rate.job;
 import kz.diploma.exchange_rate.service.IntegrationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@EnableScheduling
 @Slf4j
 public class RateScheduler {
     private final IntegrationService integrationService;
 
-    @Scheduled(cron = "0 52 01 * * ?", zone = "GMT+5")
+    @Scheduled(cron = "0 0 5 * * ?", zone = "GMT+5")
     void schedulerForSmsMonitoring() {
         log.info("Cron started");
         integrationService.getExchangeRates(null);
