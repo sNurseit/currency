ddc7baf2ecba83557bc25e2b346ebb6746c3202c
sNurseit <nurseit.taimas@gmail.com>
2024-08-16 02:05:26 +0500

    add Constants

---
+++ src/main/java/kz/diploma/exchange_rate/util/Constants.java
@@ -0,0 +1,7 @@
+package kz.diploma.exchange_rate.util;
+
+public class Constants {
+    public final static String F_DATE = "fdate";
+    public final static String USD = "USD";
+    public final static String RUB = "RUB";
+}
