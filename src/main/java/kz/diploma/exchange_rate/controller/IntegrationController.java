5faf57e0eb0e244fb32b65b5c0c5640670b77e4d
sNurseit <nurseit.taimas@gmail.com>
2024-08-16 04:15:15 +0500

    delete unnecessary endpoint

--- src/main/java/kz/diploma/exchange_rate/controller/IntegrationController.java
+++ src/main/java/kz/diploma/exchange_rate/controller/IntegrationController.java
@@ -4,24 +4,19 @@ package kz.diploma.exchange_rate.controller;
 import kz.diploma.exchange_rate.service.IntegrationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
-@Controller
+@RestController
 @RequestMapping("/api/integration")
 @RequiredArgsConstructor
 public class IntegrationController {
 
     private final IntegrationService integrationService;
-
-    @GetMapping
-    public ResponseEntity<?> getAll(){
-        return ResponseEntity.ok("OK");
-    }
 
     @PostMapping
     public ResponseEntity<?> create(@RequestParam(name = "date", required = false) LocalDate date){
